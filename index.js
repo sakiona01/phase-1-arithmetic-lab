ed00c59602cc09f9b9591f768a33b93102543ebc
Shawna Akiona <shawna.akiona@gmail.com>
2021-08-10 14:56:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 2
+const num4 = 2
+const mod = num3 + num4
+
+const max = 20
+Math.max(1,2,3,4,5);
